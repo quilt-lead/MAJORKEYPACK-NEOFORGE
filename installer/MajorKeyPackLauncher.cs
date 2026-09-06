3fe45b5cda7269062dafcf5b45ac732346d4fd86
quilt-lead <noahsamuelrosenberg@gmail.com>
2026-09-06 15:08:03 -0400

    Build standalone installer EXE

---
+++ installer/MajorKeyPackLauncher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static int Main()
+    {
+        string baseDir = AppContext.BaseDirectory;
+        string script = Path.Combine(baseDir, "Install-MajorKeyPack.ps1");
+
+        if (!File.Exists(script))
+        {
+            Console.Error.WriteLine("Install-MajorKeyPack.ps1 was not found.");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            return 1;
+        }
+
+        var psi = new ProcessStartInfo
+        {
+            FileName = "powershell.exe",
+            Arguments = "-NoProfile -ExecutionPolicy Bypass -File \"" + script + "\"",
+            UseShellExecute = true,
+            WorkingDirectory = baseDir
+        };
+
+        using var process = Process.Start(psi);
+        process.WaitForExit();
+        return process.ExitCode;
+    }
+}
